34e562c69504c2fa2705306e18c94dd3001acd95
pesta <pesta.michal29@gmail.com>
2023-12-26 14:43:51 +0100

    Past solutions added. Organised the project.

--- Solutions.cpp
+++ Solutions.cpp
@@ -1,8 +1,126 @@
-//
-// Created by pesta on 26.12.2023.
-//
-
 #include "Solutions.h"
+
+
+int Solutions::removeDuplicates(vector<int> &nums) {
+    int currElem = nums[0];
+    int count = 1;
+    for(int i = 1; i < nums.size(); i++){
+        if(nums[i] == currElem && count < 2){
+            count++;
+        }else if(nums[i] == currElem && count == 2){
+            nums.erase(nums.begin() + i);
+            i--;
+        }else{
+            count = 1;
+            currElem = nums[i];
+        }
+    }
+    return nums.size();
+}
+
+void Solutions::rotate(vector<int> &nums, int k) {
+    int n = nums.size();
+    k %= n;
+
+    int count = 0;
+    int start = 0;
+    while (count < n) {
+        int current = start;
+        int prev = nums[start];
+        do {
+            int next_index = (current + k) % n;
+            std::swap(nums[next_index], prev);
+            current = next_index;
+            count++;
+        }
+        while (current != start);
+        start++;
+    }
+
+}
+
+int Solutions::maxProfit(vector<int> &prices) {
+    int n = prices.size(), buy = prices[0], sell = 0, total = 0;
+    if(n == 1) return 0;
+    for(int i = 1;i < n; i++){
+        if(prices[i] < sell){
+            total += sell - buy;
+            buy = prices[i];
+            sell = prices[i];
+        }
+        if(prices[i] < buy){
+            buy = prices[i];
+            sell = prices[i];
+        }
+        if(prices[i] > sell) sell = prices[i];
+    }
+    total += sell - buy;
+    return total;
+}
+
+bool Solutions::canJump(vector<int> &nums) {
+    int n = nums.size(), range = nums[0];
+    for(int i = 1; i < n; i++){
+        range--;
+        if(range < 0) return false;
+        if(nums[i] > range) range = nums[i];
+    }
+    return true;
+}
+
+int Solutions::jump(vector<int> &nums) {
+    int n = nums.size(), range = nums[0], count = 0, best = nums[0];
+    if(n == 1) return 0;
+    if(n == 2) return 1;
+    for(int i = 1; i < n - 1; i++){
+        range--;
+        if(best < nums[i] + i){
+            best = nums[i] + i;
+        }
+        if(range == 0){
+            count++;
+            range = best - i;
+            best = range;
+        }
+    }
+    return count + 1;
+}
+
+int Solutions::hIndex(vector<int> &citations) {
+    int n = citations.size(), h_index = 0;
+    vector<int> count(n + 1, 0);
+    for(int i = 0; i < n; i++){
+        if(citations[i] >= n) count[n]++;
+        else count[citations[i]]++;
+    }
+    for(int i = count.size() - 2; i >= 0; i--){
+        count[i] += count[i + 1];
+    }
+    for(int i = 1; i < count.size(); i++){
+        if(count[i] >= i) h_index = i;
+    }
+
+    return h_index;
+}
+
+vector<int> Solutions::productExceptSelf(vector<int> &nums) {
+    vector<int> answer(nums.size(), 1);
+    int size = nums.size();
+
+    int left = 1;
+    for(int i = 0; i < size; i++){
+        answer[i] *= left;
+        left *= nums[i];
+    }
+
+    int right = 1;
+    for(int i = size - 1;i >= 0; i--){
+        answer[i] *= right;
+        right *= nums[i];
+    }
+
+    return answer;
+}
 
 int Solutions::canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
     int startIndex = -1;
@@ -26,3 +144,5 @@ int Solutions::canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
 
     return startIndex;
 }
+
+
--- Solutions.h
+++ Solutions.h
@@ -1,7 +1,3 @@
-//
-// Created by pesta on 26.12.2023.
-//
-
 #ifndef INTERVIEW_TOP_150_SOLUTIONS_H
 #define INTERVIEW_TOP_150_SOLUTIONS_H
 
@@ -11,7 +7,14 @@ using namespace std;
 
 class Solutions {
 public:
-    static int canCompleteCircuit(vector<int>& gas, vector<int>& cost);
+    static int removeDuplicates(vector<int>& nums); // Remove duplicates from Sorted Array
+    static void rotate(vector<int>& nums, int k); // Rotate Array
+    static int maxProfit(vector<int>& prices); //Best Time to Buy and Sell Stock ||
+    static bool canJump(vector<int>& nums); //Jump Game
+    static int jump(vector<int>& nums); //Jump Game ||
+    static int hIndex(vector<int>& citations); // H-Index
+    static vector<int> productExceptSelf(vector<int>& nums); //Product of Array Except Self
+    static int canCompleteCircuit(vector<int>& gas, vector<int>& cost); //Gas Station
 };
 
 
